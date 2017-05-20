d258ef4a4d9ba30f1ceeff2b127744acb17609df
Ravi Pidaparthi <rapida@microsoft.com>
2017-05-19 20:11:19 -0700

    Adding gulp task to automate Version bumping and release tagging

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var ts = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
 var tslint = require("gulp-tslint");
 var minify = require('gulp-minify');
+var git = require('gulp-git');
+var versionBump = require('gulp-bump')
+var tagVersion = require('gulp-tag-version');
 
 gulp.task("build", function() {
     return gulp.src([
@@ -31,3 +34,22 @@ gulp.task("build", function() {
         .pipe(minify())
         .pipe(gulp.dest('distrib'));
 });
+
+// We dont want to release anything without successful build. So build task is dependency for these tasks.
+gulp.task('patchRelease', ['build'], function() { return BumpVersionTagAndCommit('patch'); })
+gulp.task('featureRelease', ['build'], function() { return BumpVersionTagAndCommit('minor'); })
+gulp.task('majorRelease', ['build'], function() { return BumpVersionTagAndCommit('major'); })
+gulp.task('preRelease', ['build'], function() { return BumpVersionTagAndCommit('prerelease'); })
+
+function BumpVersionTagAndCommit(versionType) {
+    return gulp.src(['./package.json'])
+        // bump the version numbr
+        .pipe(versionBump({type:versionType}))
+        // save it back to filesystem 
+        .pipe(gulp.dest('./'))
+        // commit the changed version number 
+        .pipe(git.commit('Bumping package version'))
+        // tag it in the repository
+        .pipe(tagVersion());
+}
+ 
